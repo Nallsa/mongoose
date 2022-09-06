c678cd4c3ac0664b461c40c4050d2e10e44ebf8f
Aslanbek <saidtaahev@gmail.com>
2022-09-06 12:25:09 +0300

    .

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const express = require('expess')
-
+const mongoose = require('mongoose')
 const app = express()
 
 app.use(require('./router/mongoose.router'))
+
+mongoose
+  .connect(
+    'mongodb+srv://insom:ismait3310@cluster0.ybsxb7s.mongodb.net/?retryWrites=true&w=majority',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => console.log('Успешно соединились с сервером MongoDB'))
+  .catch(() => console.log('Ошибка при соединении с сервером MongoDB'))
 
 app.listen(4000, () => {
   console.log(`http://localhost:${4000}`)
